b091611b45bfd67a1c4a9ed91226fbc3f316693e
Wanderer Sada <wanderes@pr-c2.kzn.21-school.ru>
2025-01-25 01:23:27 +0300

    add home page changes

--- src/pages/catalog/Catalog.js
+++ src/pages/catalog/Catalog.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from '../../components/header/Header';
+import Footer from '../../components/footer/Footer';
 
 const Catalog = () => {
   return (
-    <div>
+    <div className="container">
+      <Header />
       <h1>Каталог товаров</h1>
       <p>Здесь будет ваш каталог товаров.</p>
+      <Footer />
     </div>
   );
 };
